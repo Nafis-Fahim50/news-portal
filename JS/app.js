951d09d60330152769476393d3dbb43ecd013467
Nafis-Fahim <nafisf96@gmail.com>
2022-09-03 16:23:34 +0600

    added news details info

--- JS/app.js
+++ JS/app.js
@@ -7,25 +7,49 @@ const loadData = async () =>{
 
 const displayData = newsPaper =>{
     newsPaper.forEach(news => {
-        console.log(news);
+        // console.log(news);
         const newsContainer = document.getElementById('news-container');
         const newsList = document.createElement('li');
         newsList.classList.add('nav-item');
         newsList.innerHTML = `
-        <a onclick="displayNews('${news.category_id}')" class="nav-link active" aria-current="page">${news.category_name}</a>
+        <a onclick="newsdetails('${news.category_id}')" class="nav-link active" aria-current="page">${news.category_name}</a>
         `
         newsContainer.appendChild(newsList);
     });
 }
 
-const displayNews = async(id)=>{
+const newsdetails = async(id)=>{
     const url = `https://openapi.programming-hero.com/api/news/category/${id}`
     const res = await fetch(url);
     const data = await res.json();
-    console.log(data.data);
+    displayNewsDetails(data.data);
 
 }
 
-
+const displayNewsDetails = details =>{
+    details.forEach(detail =>{
+        console.log(detail);
+        const newsContainer = document.getElementById('news-details');
+        // newsContainer.innerHTML = "";
+        const newsDiv = document.createElement('div');
+        newsDiv.classList.add('row');
+        newsDiv.innerHTML = `
+        <div class="col-md-4">
+            <img src="${detail.thumbnail_url}" class="img-fluid rounded-start" alt="...">
+        </div>
+        <div class="col-md-8">
+            <div class="card-body">
+            <h5 class="card-title">${detail.title}</h5>
+            <p class="card-text">${detail.details.slice(0,200)}...</p>
+            <img src="${detail.author.img}" class="img-fluid h-25 w-25 rounded-circle" alt="...">
+            <small class="text-muted pe-5">${detail.author.name}</small>
+            <small class="text-muted">${detail.total_view}</small>
+            
+            </div>
+        </div>
+        `;
+        newsContainer.appendChild(newsDiv);
+    })
+}
 
 loadData();
